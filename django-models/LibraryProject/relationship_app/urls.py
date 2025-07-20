ae541eaf575c9189be696eb77e279c87cf264a45
Abenezer daniel <abenezerdaniel06@gmail.com>
2025-07-20 15:38:31 +0300

    ww

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -12,3 +12,10 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('relationship/', include('relationship_app.urls')),
 ]
+from django.urls import path
+from .views import list_books, LibraryDetailView
+
+urlpatterns = [
+    path('books/', list_books, name='book-list'),
+    path('library/<int:pk>/', LibraryDetailView.as_view(), name='library-detail'),
+]
--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,33 +1,24 @@
 from django.shortcuts import render
 from django.views.generic import DetailView
-from .models import Book, Library, Author  # Now properly importing all needed models
+from .models import Book, Library  # Explicitly importing Library model
 
-# Function-based view to list all books
 def list_books(request):
     """
-    Function-based view that displays all books in the system
-    Returns:
-        Rendered template with list of all books and their authors
+    Function-based view listing all books with their authors
     """
-    books = Book.objects.all().select_related('author')  # Optimized query
-    return render(request, 'relationship_app/list_books.html', {'books': books})
+    books = Book.objects.all().select_related('author')
+    return render(request, 'relationship_app/list_books.html', 
+                {'books': books})
 
-# Class-based view to display library details
 class LibraryDetailView(DetailView):
     """
-    Class-based view that shows details of a specific library
-    including all books available in that library
+    Class-based view displaying details for a specific library
+    Requires Library model import to work correctly
     """
-    model = Library  # This is why we need the Library import
+    model = Library  # This line requires the Library import
     template_name = 'relationship_app/library_detail.html'
     context_object_name = 'library'
     
     def get_queryset(self):
-        """Optimized query with prefetching of related books and authors"""
+        """Optimize queries by prefetching related books"""
         return super().get_queryset().prefetch_related('books__author')
-
-    def get_context_data(self, **kwargs):
-        """Adds additional context data to the template"""
-        context = super().get_context_data(**kwargs)
-        # You can add extra context here if needed
-        return context
