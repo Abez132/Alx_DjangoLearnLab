df1d0216af33a085750106ad20b32dea44f18414
Abenezer daniel <abenezerdaniel06@gmail.com>
2025-07-20 15:35:50 +0300

    g

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,20 +1,33 @@
 from django.shortcuts import render
 from django.views.generic import DetailView
-from .models import Book, Library  # Explicitly importing Library
+from .models import Book, Library, Author  # Now properly importing all needed models
 
 # Function-based view to list all books
 def list_books(request):
-    """Function-based view that lists all books with their authors"""
+    """
+    Function-based view that displays all books in the system
+    Returns:
+        Rendered template with list of all books and their authors
+    """
     books = Book.objects.all().select_related('author')  # Optimized query
     return render(request, 'relationship_app/list_books.html', {'books': books})
 
 # Class-based view to display library details
 class LibraryDetailView(DetailView):
-    """Class-based view showing details of a specific library"""
-    model = Library  # This requires the Library import
+    """
+    Class-based view that shows details of a specific library
+    including all books available in that library
+    """
+    model = Library  # This is why we need the Library import
     template_name = 'relationship_app/library_detail.html'
     context_object_name = 'library'
     
     def get_queryset(self):
         """Optimized query with prefetching of related books and authors"""
         return super().get_queryset().prefetch_related('books__author')
+
+    def get_context_data(self, **kwargs):
+        """Adds additional context data to the template"""
+        context = super().get_context_data(**kwargs)
+        # You can add extra context here if needed
+        return context
